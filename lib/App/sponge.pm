39844d8523f875b7cd24489ce2f20bebc7644973
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-02-14 15:08:01 +0700

    Initial commit

---
+++ lib/App/sponge.pm
@@ -0,0 +1,15 @@
+package App::sponge;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+#ABSTRACT: Soak up standard input and write to a file
+
+=head1 SYNOPSIS
+
+See the included script L<sponge>.
